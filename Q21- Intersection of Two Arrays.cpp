1022793bf6730f7b8a790875eac5bf791aee3796
anand13sethi <anandsethi4@gmail.com>
2018-12-20 11:14:54 +0530

    Standard Problem.

---
+++ Q21- Intersection of Two Arrays.cpp
@@ -0,0 +1,28 @@
+/*
+Link - https://leetcode.com/problems/intersection-of-two-arrays
+*/
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> arr;
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+    int i = 0, j = 0;
+    while(i < nums1.size() && j < nums2.size()){
+        if(nums1[i] == nums2[j]){
+            if(arr.size() > 0 && arr[arr.size()-1] == nums1[i]){
+                i++;
+                j++;
+            }
+            else{
+                arr.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        else if(nums1[i] > nums2[j])
+            j++;
+        else
+            i++;
+    } 
+    return arr;
+}
